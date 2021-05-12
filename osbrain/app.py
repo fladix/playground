c25eda31c1274a5fc3706cfa5d0cf4fdeb64ae23
fladix <flavio.diniz@noaah.de>
2021-05-12 02:18:52 +0200

    PLAYG-17 Refactor core modules: app.py, world.py, and agent.py

--- osbrain/app.py
+++ osbrain/app.py
@@ -1,12 +1,11 @@
 import os
 import time
 
-from flask import Flask
-from flask import jsonify
+from flask import Flask, request
+from flask_restful import Resource, Api
 
 from noaah.world import World
 from noaah.agent import AgentUtils
-
 
 """
 Attention:
@@ -16,38 +15,67 @@ Attention:
 
 # pylint: disable=C0103
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/admin/home')
-def home():
-    msg = 'You are at home! Please look at the [Cloud Run/Debug Locally - Detailed] window'
-    return jsonify({"msg": msg}), 200
+class WorldRes(Resource):
+    
+    def get(self):
+        msg = f'Welcome to {World.get_name()}! Please look at the [Cloud Run/Debug Locally - Detailed] window'
+        return {'msg': msg}, 200
+    
+    def put(self):
+        data = request.get_json()
+        World(data['name'])
+        msg = f'Welcome to {data["name"]}!'
+        return {'msg': msg}, 200
 
-@app.route('/admin/disassemble')
-def disassemble():
-    World.disassemble_world()    
-    return jsonify({"msg": 'World has been disassembled!'}), 200
+    def delete(self):
+        World.destroy_world()    
+        return {'msg': f'{World.get_name()} was destroyed!'}, 200
 
-@app.route('/usecase/push-pull')
-def push_pull():
-    agent = AgentUtils('6uv5UhwyXmnStkYRYFdq')
-    nick = agent.get_nick()
-    ns_name = agent.get_ns_name()
-    if agent.check_alive():
-        for i in range(3):
-            agent.get_agent_proxy(ns_name).send('main', f'Hello there! This is {nick}! Message #{i}')
-            time.sleep(3)
-        msg = f'{nick} says:- Please look for messages in the console'
-    else:
-        msg = f'{nick} says:- Looks like I am dead'
-    return jsonify({"msg": msg}), 200
+
+class PushPullRes(Resource):
+    
+    def get(self):
+        agent = AgentUtils('6uv5UhwyXmnStkYRYFdq')
+        nick = agent.get_nick()
+        ns_name = agent.get_ns_name()
+        if agent.check_alive():
+            for i in range(3):
+                agent.get_agent_proxy(ns_name).send('main', f'Hello there! This is {nick}! Message #{i}')
+                time.sleep(3)
+            msg = f'{nick} says:- Please look for messages in the console'
+        else:
+            msg = f'{nick} says:- Looks like I am dead'
+        return {"msg": msg}, 200
+
+
+class AgentRes(Resource):
+
+    # TODO Get agent data
+    def get(self):
+        pass
+
+    # TODO Instantitate a new agent
+    def post(self):
+        pass
+
+    # TODO Change an agent data
+    def pu(self):
+        pass
+
+    # TODO Eliminate an agent
+    def delete():
+        pass
+
+api.add_resource(WorldRes, '/osbrain/world')
+api.add_resource(PushPullRes, '/osbrain/pushpull')
+api.add_resource(AgentRes, '/osbrain/agent/<string:agent_id>')
 
 
 if __name__ == '__main__':
 
-    # World assembling
-    world = World()
-
     # API spin-up
+    World('Westeros')
     server_port = os.environ.get('PORT', '8080')
     app.run(debug=False, port=server_port, host='0.0.0.0')
-    
--- osbrain/noaah/world.py
+++ osbrain/noaah/world.py
@@ -1,19 +1,22 @@
 """
 World creation and management routines
 """
-from cloudpickle.cloudpickle import instance
 from firebase_admin import credentials, firestore, initialize_app
-from osbrain import agent, run_agent
+from osbrain import run_agent
 from osbrain import run_nameserver
 from osbrain.address import SocketAddress
 from osbrain.proxy import NSProxy
 
 
-class World():
+class World(object):
 
-    """
-    Class public interface
-    """
+    # ----------------------
+    # Class public interface
+    # ----------------------
+    @classmethod
+    def get_name(cls):
+        return cls.__name
+
     @classmethod
     def set_db_client(cls):
         initialize_app(credentials.Certificate(cls.__key))
@@ -40,26 +43,35 @@ class World():
         pass
 
     @classmethod
-    def disassemble_world(cls):
+    def destroy_world(cls):
         for ns in cls.__live_ns:
             NSProxy(nsaddr=cls.__get_address(ns)).shutdown()
 
-    """
-    Class private interface
-    """
+    # ----------------------
+    # Class private interface
+    # ----------------------
+    __name = ''
     __db = {}
     __key = 'noaah/key.json'
     __live_ns = []
     __live_agents = []
- 
-    def __init__(self):
-        self.__class__.set_db_client()        
-        self.__instantiate_name_servers()
-        self.__instantiate_base_agents()
+    __instance = None
+
+    def __new__(cls, name):
+        if cls.__instance is None:
+            print('Creating the object')
+            cls.__instance = super(World, cls).__new__(cls)
+            # Initialization
+            cls.set_db_client()        
+            cls.__instantiate_name_servers()
+            cls.__instantiate_base_agents()
+        # Update creation time argument
+        cls.__name = name
+        return cls.__instance
 
     @classmethod
     def __instantiate_name_servers(cls):
-        print('\n', '=' * 10, 'NAME SERVERS', '=' * 10)
+        print('\n', '=' * 10, ' NAME SERVERS ', '=' * 10, sep='')
         world_ref = cls.__db.collection('world')
         for doc in world_ref.stream():
             #print(doc.to_dict())
@@ -74,35 +86,46 @@ class World():
 
     @classmethod
     def __instantiate_base_agents(cls):
-        print('\n', '=' * 10, 'AGENTS', '=' * 10)
+        print('\n', '=' * 10, ' SETING UP AGENTS ', '=' * 10, sep='')
+        print('\n', f'{"NS":<20}{"Class":<30}{"Nick":<50}{"Comms":<20}\n', '-' * 105, sep='')
         agents_ref = cls.__db.collection('agents')
         for doc in agents_ref.stream():
             dic = doc.to_dict()
             agent_proxy = run_agent(name=doc.id, nsaddr=cls.__get_address(dic['ns_name']))
             cls.__setup_agent_comms(dic, agent_proxy)
             cls.__live_agents.append(doc.id)
+            print(f'{dic["ns_name"]:<20}{dic["class"]:<30}{dic["nick"]:<50}{dic["comm"]["channels"][0]["pattern"]:<20}', sep='')
 
     @classmethod
     def __setup_agent_comms(cls, agent_data, agent_proxy):
         for channel in agent_data['comm']['channels']:
             if channel['pattern'] == 'PUSH':
-                print('PUSH:', channel)
+                #print('PUSH:', channel)
                 agent_proxy.bind('PUSH', alias=channel['alias'])
             elif channel['pattern'] == 'PULL':
-                print('PULL:', channel)
+                #print('PULL:', channel)
                 interlocutor_proxy = cls.get_agent_proxy(agent_data['ns_name'], channel['address'])
                 addr = interlocutor_proxy.addr(channel['alias'])
                 agent_proxy.connect(addr, handler=lambda agent, message: agent.log_info(f'Received: {message}')) # TODO Move out handler from here
             elif channel['pattern'] == 'PUB':
-                print('PUB:', 'TBD') # TODO Implement
+                #print('PUB:', 'TBD') # TODO Implement
+                pass
             elif channel['pattern'] == 'SUB':
-                print('SUB:', 'TBD') # TODO Implement  
+                #print('SUB:', 'TBD') # TODO Implement
+                pass  
 
 
 if __name__ == '__main__':
     
-    # Assemble World
-    world = World()
+    # Assemble World: Singleton pattern
+    world1 = World('Westeros')
+    print('\n', '=' * 10, ' REALITY TEST ', '=' * 10, sep='')
+    print(f'You are in {World.get_name()}!')
+    world2 = World('Narnia')
+    print(f'Now, rebulding as {World.get_name()}?')
+    print(f'\nBut wait, look again: \nworld1 = {world1}\nworld2 = {world2}')
 
-    # Disassemble World
-    World.disassemble_world()
+    # Destroying the world
+    print('\n', '=' * 10, ' DESTROYING SPACE-TIME ', '=' * 10, sep='')
+    print('Closing Name Servers automatically kills their linked agents')
+    World.destroy_world()
