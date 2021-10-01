6035c8090292caf669b3167268eec9a91bfd3c81
fladix <flavio.diniz@noaah.de>
2021-10-01 22:52:00 +0200

    PLAYG-33 Create full layout SPA with antd

---
+++ react/layout/src/pages/Provider.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Provider() {
+  return (
+    <div className="home">
+      <h1>Provider</h1>
+    </div>
+  );
+}
+
+export default Provider;
---
+++ react/layout/src/pages/ProviderDirectory.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ProviderDirectory() {
+  return (
+    <div className="home">
+      <h1>Provider Service Directory</h1>
+    </div>
+  );
+}
+
+export default ProviderDirectory;
